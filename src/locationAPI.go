215075a0435c90888f02659739d975ab930dbc30
James <admin@floretos.com>
2021-07-11 18:28:28 +0700

    refactor code and add more tests

---
+++ src/locationAPI.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=37.42159&longitude=-122.0837&localityLanguage=en
+
+// we just want the city from this endpoint
+type City struct {
+	City string `json:"city"`
+}
+
+func getCity(location *Location) *City {
+	url := fmt.Sprintf("https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=%flongitude=%f&localityLanguage=en", location.Latitude, location.Longitude)
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil
+	}
+	resp, err := Client.Do(request)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var city City
+	json.Unmarshal([]byte(body), &city)
+	return &city
+}
